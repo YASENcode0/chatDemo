5398cc28f183f85997349108c3cecf83405e746c
YASENcode0 <easeneaseneasen123@gmail.com>
2024-05-25 16:31:11 +0300

    add sokit io

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const cors = require("cors");
 const app = express();
 const PORT = 3001 || 5000;
 const Note = require("./Models/Notes");
+const http = require("http");
+const socketIo = require("socket.io");
 
 app.use(cors());
 app.use(express.json());
+
+const server = http.createServer(app);
+const io = socketIo(server);
 
 mongoose
   .connect(
@@ -21,6 +26,35 @@ mongoose
     console.log("err db ", err);
   });
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("addNote", async (data) => {
+    try {
+      const newNote = new Note();
+      newNote.__id = data.__id;
+      newNote.content = data.content;
+      await newNote.save();
+
+      io.emit("noteAdded", newNote); // إرسال الملاحظة الجديدة لجميع العملاء
+    } catch (err) {
+      console.error("Error adding note:", err);
+    }
+  });
+
+  socket.on("getAllNotes", async () => {
+    try {
+      const notes = await Note.find();
+      socket.emit("allNotes", notes); // إرسال جميع الملاحظات للعميل
+    } catch (err) {
+      console.error("Error getting notes:", err);
+    }
+  });
+});
 app.get("/getall", async (req, res) => {
   try {
     await Note.find().then((response) => {
@@ -40,9 +74,8 @@ app.post("/addone", async (req, res) => {
     newNote.content = content;
 
     await newNote.save();
-    res.json(newNote)
+    res.json(newNote);
   } catch (err) {
     res.send("err add one").status(500);
   }
 });
-
