80ddd1a72efd906f0bf2115a68d8b6ebafcbf985
YASENcode0 <easeneaseneasen123@gmail.com>
2024-05-25 16:34:47 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
+const Note = require('./Models/Notes');
+
 const app = express();
-const PORT = 3001 || 5000;
-const Note = require("./Models/Notes");
-const http = require("http");
-const socketIo = require("socket.io");
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -18,55 +19,55 @@ mongoose
     "mongodb+srv://steve:steve123steve@firstcluster.d4jodqk.mongodb.net/?retryWrites=true&w=majority&appName=firstCluster"
   )
   .then(() => {
-    app.listen(PORT, () => {
-      console.log("listen on port ", PORT);
+    server.listen(PORT, () => { // استخدم server.listen بدلاً من app.listen
+      console.log("Server is running on port", PORT);
     });
   })
   .catch((err) => {
-    console.log("err db ", err);
+    console.log("Database connection error:", err);
   });
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
   });
 
-  socket.on("addNote", async (data) => {
+  socket.on('addNote', async (data) => {
     try {
       const newNote = new Note();
       newNote.__id = data.__id;
       newNote.content = data.content;
       await newNote.save();
 
-      io.emit("noteAdded", newNote); // إرسال الملاحظة الجديدة لجميع العملاء
+      io.emit('noteAdded', newNote); // إرسال الملاحظة الجديدة لجميع العملاء
     } catch (err) {
       console.error("Error adding note:", err);
     }
   });
 
-  socket.on("getAllNotes", async () => {
+  socket.on('getAllNotes', async () => {
     try {
       const notes = await Note.find();
-      socket.emit("allNotes", notes); // إرسال جميع الملاحظات للعميل
+      socket.emit('allNotes', notes); // إرسال جميع الملاحظات للعميل
     } catch (err) {
       console.error("Error getting notes:", err);
     }
   });
 });
-app.get("/getall", async (req, res) => {
+
+// مسارات Express
+app.get('/getall', async (req, res) => {
   try {
-    await Note.find().then((response) => {
-      console.log(response);
-      res.json({ response });
-    });
+    const notes = await Note.find();
+    res.json({ notes });
   } catch (err) {
-    res.send("err gat all").status(500);
+    res.status(500).send("Error getting all notes");
   }
 });
 
-app.post("/addone", async (req, res) => {
+app.post('/addone', async (req, res) => {
   try {
     const { __id, content } = req.body;
     const newNote = new Note();
@@ -75,7 +76,13 @@ app.post("/addone", async (req, res) => {
 
     await newNote.save();
     res.json(newNote);
+
+    io.emit('noteAdded', newNote); // إرسال الملاحظة الجديدة لجميع العملاء
   } catch (err) {
-    res.send("err add one").status(500);
+    res.status(500).send("Error adding note");
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
